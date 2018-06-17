86ecbb0141a2c2fde337ac4a697e5fa02e3587c7
cwilliams3579 <clevelandwilliams3579@gmail.com>
2018-06-17 16:45:35 -0400

    add sound to lion

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length: {minimum: 10}
+  validates :user_id, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length: {minimum: 10}
+  validates :user_id, presence: true
 end
