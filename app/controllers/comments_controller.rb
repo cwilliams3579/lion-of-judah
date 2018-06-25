c28ba168a08c4554b8eaa58c8088903d291a6f94
Cleveland Williams <clevelandwilliams3579@gmail.com>
2018-06-25 05:47:56 +0000

    add comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,17 @@
 class CommentsController < ApplicationController
     def create
         @post = Post.find(params[:post_id])
-        @comment = @post.comment.create(params[:comment].permit(:description))
+        @comment = @post.comments.create(params[:comment].permit(:description))
         redirect_to post_path(@post)
     end
 
-    def show
-        
-    end
+  
 
     def destroy
-        
+       @post = Post.find(params[:post_id]) 
+       @comment = @post.comment.find(params[:id]
+       @comment.destroy
+       redirect_to post_path(@post)
     end
     
     
