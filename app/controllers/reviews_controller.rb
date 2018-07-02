e70a1490917d11d98321571fb8445ca2a0905143
EC2 Default User <ec2-user@ip-172-31-6-209.us-east-2.compute.internal>
2018-07-02 04:44:52 +0000

    Set up links and routing for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -22,7 +22,7 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to books_path, notice: 'Review was successfully created.' }
+        format.html { redirect_to @book, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
