a412b6803f1cf60a03bb47a8da4ce6e87b7505dc
cwilliams3579 <clevelandwilliams3579@gmail.com>
2018-07-13 16:55:52 +0000

    update from aws cloud9

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,6 +1,10 @@
-# ENV["ELASTICSEARCH_URL"] = "https://es-domain-1234.us-east-1.es.amazonaws.com"
-# Searchkick.aws_credentials = {
-#   access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-#   secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-#   region: "us-east-1"
-# }
+# Searchkick.client = Elasticsearch::Client.new({
+#   url: ENV["ELASTICSEARCH_URL"],
+#   transport_options: { request: { timeout: timeout }, headers: { content_type: "application/json" } }
+# }.deep_merge(client_options)) do |f|
+#   f.use Searchkick::Middleware
+#   f.request :aws_signers_v4,
+#     credentials: Aws::InstanceProfileCredentials.new,
+#     service_name: "es",
+#     region: "us-east-1"
+# end
